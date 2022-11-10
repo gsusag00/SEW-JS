c4678a1b6e1f6855f0e724a697ed50f8f8d6e791
gsusag00 <jesus.ag00@gmail.com>
2022-11-10 20:23:34 +0100

    No se que coño hacer con el mrc y el m+ y el m- de los cojones

--- Seccion 1/Ejercicio-03/CalculadoraMilan.js
+++ Seccion 1/Ejercicio-03/CalculadoraMilan.js
@@ -1,39 +1,57 @@
 class CalculadoraMilan {
 
+    op = '';
+    left = '';
+    right = '';
+    point = false;
+    has_eval = false;
+    memory = 0;
+
     constructor() {
         this.init();
     }
 
     init() {
-        this.screen = '';
         this.op = '';
-        this.left_op = '';
+        this.left = '';
+        this.right = '';
         this.point = false;
         this.memory = 0;
     }
 
     operacion(val) {
+        if(this.right) {
+            this.igual();
+        }
         this.op = val;
         this.update_screen();
     }
 
     numeros(val) {
-        if (this.op === '') {
-            if(val === '.') {
-                if (!isNaN(this.left_op.slice(-1)) && !this.point) {
-                    this.left_op += val;
-                    this.point = true;
-                }
+        if (!this.op) {
+            if(this.has_eval) {
+                this.left = val.toString();
+                this.has_eval = false;
+            } else {
+                this.left += val.toString();
             }
-            else 
-                this.left_op += val;
-
-        } 
-        else {
-            this.left_op += this.op + val;
-            this.point = false;
         }
-        this.op = '';
+        else {
+            this.right += val.toString();
+        }
+        this.update_screen();
+    }
+
+    punto() {
+        if(this.right) {
+            if(!this.right.includes('.')) {
+                this.right += '.';
+            }
+        } else {
+            if(!this.left.includes('.')){
+                this.left += '.';
+            }
+        }
         this.update_screen();
     }
 
@@ -45,21 +63,54 @@ class CalculadoraMilan {
     cepress() {
         this.screen = '';
         this.op = '';
-        this.left_op = '';
+        this.left = '';
         this.point = false;
         this.update_screen();
     }
 
     changesign() {
-
+        if(this.right) {
+            if(this.right.charAt(0) === '-') {
+                this.right = this.right.substring(1);
+            }
+            else {
+                this.right = '-' + this.right;
+            }
+        }
+        else {
+            if(this.left.charAt(0) === '-') {
+                this.left = this.left.substring(1);
+            }
+            else {
+                this.left = '-' + this.left;
+            }
+        }
+        this.update_screen();
     }
 
-    squareroot() {
-
+    sqrt() {
+        if(this.right) {
+            this.right = 'Math.sqrt(' + this.right + ')';
+        }
+        else {
+            this.left = 'Math.sqrt(' + this.left + ')';
+        }
+        this.update_screen();
     }
 
-    modulus() {
-
+    porcentage() {
+        if(this.right) {
+            if(this.op === '+' || this.op === '-') {
+                this.right = '(' + this.left + '/ 100 ) * ' + this.right;
+            }
+            else {
+                this.right = '(' + this.right + ' / 100 )'; 
+            }
+        }
+        else {
+            this.left = '( ' + this.left + ' / 100)'
+        }
+        this.igual();
     }
 
     mrcpress() {
@@ -76,7 +127,8 @@ class CalculadoraMilan {
 
     igual() {
         try {
-            var toEval = eval(this.left_op + this.op);
+            var toEval = eval(this.left + this.op + this.right);
+            this.has_eval = true;
             this.update_screen(toEval);
         } catch (err) {
             this.screen = 'Error';
@@ -85,19 +137,24 @@ class CalculadoraMilan {
     }
 
     update_screen(toEval=null) {
+        var val;
         if(toEval === null) {
-            this.screen = this.left_op + this.op;
+            val = this.left + this.op + this.right;
         }
         else {
-            this.screen = toEval;
-            this.left_op = toEval;
-            this.op = '';
+            val = toEval;
         }
         if(this.screen === 'Error') {
             this.init();
             this.update_screen();
         }
-        document.getElementById('pantalla').value = this.screen;
+        if(this.right && toEval !== null) {
+            this.left = val.toString();
+            this.op = '';
+            this.right = '';
+        }
+        document.getElementById('pantalla').value = val;
+        console.log('Left value: ' + this.left);
     }
 
 }
@@ -111,7 +168,19 @@ document.addEventListener('keydown',function (event) {
     if(ops.includes(event.key)) {
         calc.operacion(event.key);
     }
-    if(event.key === 'Enter') {
+    if(event.key === '=') {
         calc.igual();
+    }
+    if(event.key === 's') {
+        calc.sqrt();
+    }
+    if(event.key === '%') {
+        calc.porcentage();
+    }
+    if(event.key === 'c') {
+        calc.cpress();
+    }
+    if(event.key === 'e') {
+        calc.cepress();
     }
 });
