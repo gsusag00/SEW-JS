49499649c1a88b38ba3dfd3ae13766b5739028ea
gsusag00 <jesus.ag00@gmail.com>
2022-11-21 14:56:05 +0100

    Unbothered. Moisturized. Happy. In my lane. Focused. Flourishing

--- Seccion-2/Ejercicio-07/Ejercicio7.js
+++ Seccion-2/Ejercicio-07/Ejercicio7.js
@@ -11,6 +11,28 @@ class OcultarElementos {
         $(element).hide();
     }
 
+    modify() {
+        $('img').attr('src',"bombilla.jpg");
+        $('img').attr('alt',"Una bombilla");
+    }
+
+    restore() {
+        $('img').attr('src',"vela.jpg");
+        $('img').attr('alt',"Una vela");
+    }
+
+    deleteRow() {
+        var length = $('table tr').length;
+        if(length > 0) {
+            var row = $('table tr').get(length-1)
+            row.remove();
+        }
+    }
+
+    addRow() {
+
+    }
+
 }
 
 var oc = new OcultarElementos();
