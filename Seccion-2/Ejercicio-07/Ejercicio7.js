987e37efc8b7ad8eccce896ff5a13a25934c51ad
gsusag00 <jesus.ag00@gmail.com>
2022-11-28 14:09:04 +0100

    Balling in the woodworks

--- Seccion-2/Ejercicio-07/Ejercicio7.js
+++ Seccion-2/Ejercicio-07/Ejercicio7.js
@@ -30,7 +30,33 @@ class OcultarElementos {
     }
 
     addRow() {
+        $('table').append('<tr><td> ' + Math.floor(Math.random() * 10) + 
+                            '</td><td>' + Math.floor(Math.random() * 10) + 
+                            '</td><td>' +  Math.floor(Math.random() * 10) + 
+                            '</td><td>' +  Math.floor(Math.random() * 10) + 
+                            '</td><td>' +  Math.floor(Math.random() * 10)  + 
+                            '</td></tr>')
+    }
 
+    sumaTabla() {
+        var count = 0;
+        $('table tr td').each(function() {
+            var celda = $.trim($(this).text());
+            if(!isNaN(celda)) {
+                count += Number(celda);
+            }
+        })
+        var prevText = $('h3').text();
+        var text = prevText.split(':')
+
+        $('h3').text(text[0] + ': ' + count.toString());console.log(count);
+    }
+
+    recorrerDOM() {
+        $('*',document.body).each(function() {
+            var padre = $(this).parent().get(0).tagName;
+            $(this).prepend(document.createTextNode("Etiqueta padre : < " + padre + " > elemento : < " + $(this).get(0).tagName + " > valor: "))
+        })
     }
 
 }
